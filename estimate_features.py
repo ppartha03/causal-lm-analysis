5e83bb3b38d8964fa0035882ae63ed6cf5853b77
Prasanna Parthasarathi <pparth2@narval2.narval.calcul.quebec>
2022-04-07 11:16:48 -0400

    final

--- estimate_features.py
+++ estimate_features.py
@@ -12,26 +12,31 @@ from itertools import product
 from pathlib import Path
 from datetime import datetime
 
-def get_features(text, model_name, batch_size=16):
+def get_features(text, model_name, target_dir, batch_size=16):
     model_dir = "/home/pparth2/scratch/causal-lm-project/causal-lm-analysis/cached"
     tokenizer = AutoTokenizer.from_pretrained(os.path.join(model_dir, model_name))
     model = AutoModel.from_pretrained(os.path.join(model_dir, model_name), pad_token_id=tokenizer.eos_token_id).to(torch.device('cpu'))
     model = model.eval()
     tokenizer.pad_token = tokenizer.eos_token
-    feats = []
+    #feats = []
     device = model.device
-    for i in tqdm(range(0,len(text), batch_size)):
-      tokenized_texts = tokenizer.batch_encode_plus(text[i:i+batch_size], return_tensors='pt', truncation=True, max_length=256, padding=True, add_special_tokens=False)['input_ids']
-      if isinstance(tokenized_texts, list):
-        tokenized_texts = torch.LongTensor(tokenized_texts).unsqueeze(0)
-      out = model(input_ids=tokenized_texts.to(device), past_key_values=None,
-                     output_hidden_states=True, return_dict=True)
-      h = out.hidden_states[-1]  # (batch_size, seq_len, dim)
-      feats.append(h[:, -1, :].cpu())
+
+    feat_file = os.path.join(target_dir, 'features.npy')
+    with open(feat_file, 'w') as f:
+        for i in tqdm(range(0,len(text), batch_size)):
+          tokenized_texts = tokenizer.batch_encode_plus(text[i:i+batch_size], return_tensors='pt', truncation=True, max_length=256, padding=True, add_special_tokens=False)['input_ids']
+          if isinstance(tokenized_texts, list):
+            tokenized_texts = torch.LongTensor(tokenized_texts).unsqueeze(0)
+          out = model(input_ids=tokenized_texts.to(device), past_key_values=None,
+                         output_hidden_states=True, return_dict=True)
+          h = out.hidden_states[-1]  # (batch_size, seq_len, dim)
+          for row in h[:, -1, :].detach().cpu().numpy():
+              np.savetxt(f, [row])
+
 
     del tokenizer
     del model
-    return torch.cat(feats).detach().cpu().numpy()
+    return None
 
 def HyperFeatures(config):
     '''
@@ -39,7 +44,6 @@ def HyperFeatures(config):
     blimp: 'island_effects', 'anaphor_agreement', 's-selection', 'argument_structure', 'determiner_noun_agreement', 'subject_verb_agreement', 'ellipsis',
  'control_raising', 'quantifiers', 'irregular_forms', 'npi_licensing', 'binding', 'filler_gap_dependency'
     '''
-
     if config['dataset'] == 'universal_dependencies':
         dataset = load_dataset("universal_dependencies", config['sub_dataset'])[config['split']]['text']
     elif config['dataset'] == 'blimp':
@@ -52,9 +56,9 @@ def HyperFeatures(config):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    features = get_features(dataset, config['model'], batch_size=config['batch_size'])
+    features = get_features(dataset, config['model'], target_dir, batch_size=config['batch_size'])
 
-    np.save(os.path.join(target_dir, 'features.npy'), features)
+    #np.save(os.path.join(target_dir, 'features.npy'), features)
 
     return None
 
@@ -68,7 +72,7 @@ if __name__ == '__main__':
  }
     splits = {
     'en_pronouns': ['test'],
-    'en_esl': ['train', 'validation', 'test'],
+    'en_esl': ['train', 'validation', 'test'], #train
     'en_ewt': ['train', 'validation', 'test'],
     'en_gum': ['train', 'validation', 'test'],
     'en_gumreddit': ['train', 'validation', 'test'],
@@ -77,7 +81,7 @@ if __name__ == '__main__':
     'en_pud': ['test']
     }
     PARAM_GRID = list(product(
-    ['gpt2-large', 'gpt2'],
+    ['gpt2'],
     ['universal_dependencies', 'blimp']
     )
     )
@@ -93,9 +97,9 @@ if __name__ == '__main__':
         for sub_data in sub_datasets[dataset]:
             if 'blimp' not in dataset:
                 for split in splits[sub_data]:
-                    h_param_list.append({'dataset':dataset, 'sub_dataset': sub_data, 'split': split, 'model': model, 'batch_size':16})
+                    h_param_list.append({'dataset':dataset, 'sub_dataset': sub_data, 'split': split, 'model': model, 'batch_size':4})
             else:
-                h_param_list.append({'dataset':dataset, 'sub_dataset': sub_data, 'model': model, 'batch_size':16})
+                h_param_list.append({'dataset':dataset, 'sub_dataset': sub_data, 'model': model, 'batch_size':4, 'split': 'test'})
 
     # run by submitit
     d = datetime.today()
@@ -112,7 +116,7 @@ if __name__ == '__main__':
     workers_per_gpu = 10
     executor = submitit.AutoExecutor(folder=submitit_logdir)
     executor.update_parameters(
-        timeout_min=120,
+        timeout_min=5,
         gpus_per_node=num_gpus,
         slurm_additional_parameters={"account": "rrg-bengioy-ad"},
         tasks_per_node=num_gpus,
@@ -120,5 +124,5 @@ if __name__ == '__main__':
         slurm_mem="32G",#16G
         slurm_array_parallelism=100,
     )
-    job = executor.map_array(HyperFeatures, h_param_list)
+    job = executor.map_array(HyperFeatures, [h_param_list[0]])
     print(str(len(h_param_list)) + ' Jobs submitted!')
--- pairwise_mauve.py
+++ pairwise_mauve.py
@@ -6,13 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+from itertools import product
+from pathlib import Path
+from datetime import datetime
+
+
+import submitit
 import os
+import pandas as pd
+
+import spacy
+import json
+nlp = spacy.load("en_core_web_sm")
 
 
 def get_nouns_verbs(sentences):
   nouns = {}
   verbs = {}
-  for s in a_d:
+  for s in sentences:
     toks = nlp(s)
     for np in toks.noun_chunks:
       if str(np).lower() in nouns:
@@ -38,43 +49,45 @@ def plot_histograms(config, out):
     plt.bar(x, sample_p, color='blue', alpha=0.3, label='P')
     plt.bar(x, sample_q, color='red', alpha=0.3, label='Q')
     plt.legend()
-    plt.title(config['dataset1']+'_'+config['sub_dataset1']+'_and_'+config['dataset2']+'-'+config['sub_dataset2'])
+    plt.title(config['dataset1']+'_'+config['sub_dataset1']+'_' + config['split'] + '_and_'+config['dataset2']+'_'+config['sub_dataset2'])
 
-    plt.savefig(os.path.join('Mauve', config['model'], config['dataset1']+'_'+config['sub_dataset1']+'_and_'+config['dataset2']+'-'+config['sub_dataset2']), 'clusters_histograms.png')
+    plt.savefig(os.path.join('Mauve', config['model'], config['dataset1']+'_'+config['sub_dataset1']+ '_' + config['split'] + '_and_'+config['dataset2']+'_'+config['sub_dataset2'], 'clusters_histograms.png'))
     plt.close()
 
     return None
 
 def plot_and_save(config, out, arg = 'noun'):
     plt.plot(out.divergence_curve[:, 0], out.divergence_curve[:, 1])
-    plt.title(config['dataset1']+'_'+config['sub_dataset1']+'_and_'+config['dataset2']+'-'+config['sub_dataset2'])
+    plt.title(config['dataset1'] + '_' + config['sub_dataset1'] + '_' + config['split'] + '_and_' + config['dataset2'] + '_' + config['sub_dataset2'])
 
-    plt.savefig(os.path.join('Mauve', config['model'], config['dataset1']+'_'+config['sub_dataset1']+'_and_'+config['dataset2']+'-'+config['sub_dataset2']), arg + '_divergence.png')
+    plt.savefig(os.path.join('Mauve', config['model'], config['dataset1'] + '_' + config['sub_dataset1'] + '_' + config['split'] + '_and_' + config['dataset2'] + '_' + config['sub_dataset2'], arg + '_divergence.png'))
     plt.close()
 
     return None
 
 
 def HyperMauve(config):
-    target_dir = os.path.join('Mauve', config['model'], config['dataset1']+'_'+config['sub_dataset1']+'_and_'+config['dataset2']+'-'+config['sub_dataset2'])
+    target_dir = os.path.join('Mauve', config['model'], config['dataset1'] + '_' + config['sub_dataset1'] + '_' + config['split'] + '_and_' + config['dataset2'] + '_' + config['sub_dataset2'])
 
-    p_features_s = np.load(os.path.join('Features', config['model'], config['dataset1'], config['sub_dataset1'], config['split1'], 'features.npy'))
-    q_features_s = np.load(os.path.join('Features', config['model'], config['dataset2'], config['sub_dataset2'], 'features.npy'))
+    p_features_s = np.loadtxt(os.path.join('Features', config['model'], config['dataset1'], config['sub_dataset1'], config['split'], 'features.npy'), dtype=float)
+    q_features_s = np.loadtxt(os.path.join('Features', config['model'], config['dataset2'], config['sub_dataset2'], 'test', 'features.npy'), dtype=float)
 
     p_text = load_dataset("universal_dependencies", config['sub_dataset1'])[config['split']]['text']
-    blimp_df = pd.read_csv('./blimp/blimp.csv')
+    q_text = load_dataset("universal_dependencies", config['sub_dataset2'])[config['split']]['text']
+    #blimp_df = pd.read_csv('./blimp/blimp.csv')
 
-    q_text = blimp_df[blimp_df.linguistics_term == config['sub_dataset']]["sentence_bad"].tolist() + blimp_df[blimp_df.linguistics_term == config['sub_dataset']]["sentence_good"].tolist()
+    # q_text = blimp_df[blimp_df.linguistics_term == config['sub_dataset2']]["sentence_bad"].tolist() + blimp_df[blimp_df.linguistics_term == config['sub_dataset2']]["sentence_good"].tolist()
+    # p_text = blimp_df[blimp_df.linguistics_term == config['sub_dataset1']]["sentence_bad"].tolist() + blimp_df[blimp_df.linguistics_term == config['sub_dataset1']]["sentence_good"].tolist()
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
     p_nouns, p_verbs = get_nouns_verbs(p_text)
     q_nouns, q_verbs = get_nouns_verbs(q_text)
 
-    out_nouns = discrete_mauve(p_nouns, q_nouns)
-    out_verbs = discrete_mauve(p_verbs, q_verbs)
+    out_nouns = discrete_mauve.discrete_mauve(p_nouns, q_nouns)
+    out_verbs = discrete_mauve.discrete_mauve(p_verbs, q_verbs)
 
-    out = compute_mauve(p_features=p_features_s, q_features=q_features_s,
+    out = compute_mauve.compute_mauve(p_features=p_features_s, q_features=q_features_s,
                     device_id=0, max_text_length=256)
 
     plot_and_save(config, out_nouns, arg = 'noun')
@@ -86,17 +99,18 @@ def HyperMauve(config):
     # write cluster samples and metrics
 
     cluster_file = os.path.join(target_dir, 'cluster_details.json')
+    mauve_scores_file = os.path.join(target_dir, 'mauve_scores.json')
     clusters = {}
 
-    refs = {'p': config['dataset1'] + '_' + config['sub_dataset1'] + '_' + config['split1'], 'q': config['dataset2'] + '_' +config['sub_dataset2']}
+    refs = {'p': config['dataset1'] + '_' + config['sub_dataset1'] + '_' + config['split'], 'q': config['dataset2'] + '_' +config['sub_dataset2']}
     for cluster_id, samples in out.cluster_stats.items():
         clusters[cluster_id] = {refs[k]: points for k, points in samples.items()}
 
-    json.dump(open(cluster_file), out.cluster_stats)
+    json.dump(clusters, open(cluster_file, 'w'))
 
     mauve_scores = {'mauve_features': out.mauve, 'mauve_nouns': out_nouns.mauve, 'mauve_verbs': out_verbs.mauve}
 
-    json.dump(open(cluster_file), out.cluster_stats)
+    json.dump(mauve_scores, open(mauve_scores_file, 'w'))
 
     return None
 
@@ -120,12 +134,13 @@ if __name__ == '__main__':
     }
 
     PARAM_GRID = list(product(
-    ['gpt2-large', 'gpt2'],
+    ['gpt2'],
     ['universal_dependencies'],
-    ['blimp'],
-    ['en_pronouns', 'en_esl', 'en_ewt', 'en_gum', 'en_gumreddit', 'en_lines', 'en_partut', 'en_pud'],
-    ['island_effects', 'anaphor_agreement', 's-selection', 'argument_structure', 'determiner_noun_agreement', 'subject_verb_agreement', 'ellipsis',
- 'control_raising', 'quantifiers', 'irregular_forms', 'npi_licensing', 'binding', 'filler_gap_dependency']
+    ['universal_dependencies'], #['blimp']
+    ['en_ewt', 'en_gum', 'en_lines', 'en_partut'],#['island_effects', 'anaphor_agreement', 's-selection', 'argument_structure', 'determiner_noun_agreement', 'subject_verb_agreement', 'ellipsis',
+ #'control_raising', 'quantifiers', 'irregular_forms', 'npi_licensing', 'binding', 'filler_gap_dependency'],#['en_pronouns', 'en_esl', 'en_ewt', 'en_gum', 'en_gumreddit', 'en_lines', 'en_partut', 'en_pud'],
+    ['en_ewt', 'en_gum', 'en_lines', 'en_partut'],#['island_effects', 'anaphor_agreement', 's-selection', 'argument_structure', 'determiner_noun_agreement', 'subject_verb_agreement', 'ellipsis',
+ #'control_raising', 'quantifiers', 'irregular_forms', 'npi_licensing', 'binding', 'filler_gap_dependency']
     )
     )
 
@@ -137,8 +152,8 @@ if __name__ == '__main__':
 
         model, dataset1, dataset2, sub_data1, sub_data2 = params
 
-        for sub_data in sub_dataset[dataset1]:
-            h_param_list.append({'dataset1': dataset1, 'sub_dataset1': sub_data1, 'dataset2': dataset2, 'sub_dataset2': sub_data2, 'split': splits[sub_data1], 'model': model})
+        #for split in splits[sub_data1]:
+        h_param_list.append({'dataset1': dataset1, 'sub_dataset1': sub_data1, 'dataset2': dataset2, 'sub_dataset2': sub_data2, 'split': 'train', 'model': model})
 
 
     # run by submitit
@@ -153,16 +168,16 @@ if __name__ == '__main__':
     exp_dir.mkdir(parents=True, exist_ok=True)
     submitit_logdir = exp_dir / "submitit_logs"
     num_gpus = 1
-    workers_per_gpu = 10
+    workers_per_gpu = 5
     executor = submitit.AutoExecutor(folder=submitit_logdir)
     executor.update_parameters(
-        timeout_min=100,
-        gpus_per_node=num_gpus,
+        timeout_min=60,
+        #gpus_per_node=num_gpus,
         slurm_additional_parameters={"account": "rrg-bengioy-ad"},
-        tasks_per_node=num_gpus,
+        #tasks_per_node=num_gpus,
         cpus_per_task=workers_per_gpu,
         slurm_mem="32G",
         slurm_array_parallelism=100,
     )
     job = executor.map_array(HyperMauve, h_param_list)
-    print('Jobs submitted!')
+    print(str(len(h_param_list)) + ' Jobs submitted!')
